150adde91af28a97f8baf2fa6f486dbfeca7dc26
cornet <cornet@sheepy.org>
2012-12-11 02:01:59 +0000

    Working !

--- lib/graphite_template/template_handler.rb
+++ lib/graphite_template/template_handler.rb
@@ -1,15 +1,10 @@
-require 'active_support/core_ext'
 require 'graphite_graph'
 
 module GraphiteTemplate
   class TemplateHandler
-    # Return string of ruby to evaluate the template
     def self.call(template)
-      if template.sounce.is_a? String
-        # TODO: need to handle this
-      else
-        "GraphiteGraph.new(template.source).url"
-      end
+      "GraphiteGraph.new('#{template.inspect}').url"
+      # "'#{template.inspect}'"
     end
   end
 end
