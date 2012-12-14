d1c521936d05ccc3226f3c233a1f4fec7b7c8b8e
Chris Andrews <chris@nodnol.org>
2012-12-14 14:35:29 +0000

    Allow the graph format to be set as a local.

--- lib/graphite_template/template_handler.rb
+++ lib/graphite_template/template_handler.rb
@@ -3,8 +3,7 @@ require 'graphite_graph'
 module GraphiteTemplate
   class TemplateHandler
     def self.call(template)
-      "'#{GraphiteTemplate::Config.graphite_url}?' + GraphiteGraph.new('#{template.inspect}', {}, assigns.merge(local_assigns)).url"
+      "'#{GraphiteTemplate::Config.graphite_url}?' + GraphiteGraph.new('#{template.inspect}', {}, assigns.merge(local_assigns)).url(local_assigns['format'])"
     end
-
   end
 end
